d6e72d14437949145e06651a680c3093e1e199b9
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-07-25 21:44:07 +0530

    Search operation

--- main/BSTMain.java
+++ main/BSTMain.java
@@ -62,6 +62,24 @@ public class BSTMain {
         } 
     } 
     
+    boolean search(int key)  { 
+        root = search_Recursive(root, key); 
+        if (root!= null)
+            return true;
+        else
+            return false;
+    }
+    
+    Node search_Recursive(Node root, int key)  { 
+        // Base Cases: root is null or key is present at root 
+        if (root==null || root.key==key) 
+            return root; 
+        // val is greater than root's key 
+        if (root.key > key) 
+            return search_Recursive(root.left, key); 
+        // val is less than root's key 
+        return search_Recursive(root.right, key); 
+    } 
 	
 	public static void main(String[] args) {
 		BSTMain bst = new BSTMain();
@@ -69,9 +87,11 @@ public class BSTMain {
 		int i=0;
 		do {
 			System.out.println("Enter 1 for add data");
+			System.out.println("Enter 2 for search data");
 			i = scan.nextInt();
 			if(i == 1)
 			{
+				
 				System.out.println("Enter the data to be insert");
 				int x = scan.nextInt();
 				bst.insert(x);
@@ -80,8 +100,17 @@ public class BSTMain {
 		        System.out.println("\n");
 		        i=0;
 			}
+			else if(i == 2)
+			{
+				System.out.println("Enter the data to be search");
+				int x = scan.nextInt();
+				boolean ret_val = bst.search(x);
+				ret_val = bst.search(x);
+		        System.out.println("Enter number is present this statment is:-"+ret_val);
+		        i=0;
+			}
 		}while(i == 0);
         
-	}
 
+	}
 }
